6352b3a256765a9ce3f14d19466127641e43f6cf
pedro <pedrohenriqueabrantes.ph@gmail.com>
2025-05-23 19:53:38 -0300

    entity hospital

--- src/main/java/ifsc/poo3/prova/entities/hospital.java
+++ src/main/java/ifsc/poo3/prova/entities/hospital.java
@@ -1,4 +1,13 @@
 package ifsc.poo3.prova.entities;
 
 public class hospital {
+    private String name;
+    private String street_name;
+    private short street_number;
+    private String neighbourhood;
+    private String city;
+    private String state;
+    private String postal_code;
+    private int phone_number;
+    private String full_address;
 }
