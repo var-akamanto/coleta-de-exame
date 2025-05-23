9b72277a14f884fc3a028dc736cd4f80bd4e08ee
pedro <pedrohenriqueabrantes.ph@gmail.com>
2025-05-23 20:01:13 -0300

    entity hospital's getters & setters

--- src/main/java/ifsc/poo3/prova/entities/hospital.java
+++ src/main/java/ifsc/poo3/prova/entities/hospital.java
@@ -10,4 +10,75 @@ public class hospital {
     private String postal_code;
     private int phone_number;
     private String full_address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet_name() {
+        return street_name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+
+    public short getStreet_number() {
+        return street_number;
+    }
+
+    public void setStreet_number(short street_number) {
+        this.street_number = street_number;
+    }
+
+    public String getNeighbourhood() {
+        return neighbourhood;
+    }
+
+    public void setNeighbourhood(String neighbourhood) {
+        this.neighbourhood = neighbourhood;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getFull_address() {
+        return street_name  + "," +  street_number + ","
+                + neighbourhood + "," + city +
+                "/" + state +
+                "," + postal_code;
+    }
 }
--- src/main/java/ifsc/poo3/prova/entities/user.java
+++ src/main/java/ifsc/poo3/prova/entities/user.java
@@ -10,4 +10,63 @@ public class user {
     private String register_id;
     private String login_user;
     private String login_password;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getRegister_id() {
+        return register_id;
+    }
+
+    public void setRegister_id(String register_id) {
+        this.register_id = register_id;
+    }
+
+    public String getLogin_user() {
+        return login_user;
+    }
+
+    public void setLogin_user(String login_user) {
+        this.login_user = login_user;
+    }
+
+    public String getLogin_password() {
+        return login_password;
+    }
+
+    public void setLogin_password(String login_password) {
+        this.login_password = login_password;
+    }
 }
+
+
