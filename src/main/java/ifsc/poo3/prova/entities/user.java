bb843a8f7ee6007025b0d0159cd0d1396bf97464
pedro <pedrohenriqueabrantes.ph@gmail.com>
2025-05-23 19:45:01 -0300

    entity user

--- src/main/java/ifsc/poo3/prova/entities/user.java
+++ src/main/java/ifsc/poo3/prova/entities/user.java
@@ -1,4 +1,13 @@
 package ifsc.poo3.prova.entities;
 
+import java.util.Date;
+
 public class user {
+    private String name;
+    private char gender;
+    private short age;
+    private Date birth_date;
+    private String register_id;
+    private String login_user;
+    private String login_password;
 }
